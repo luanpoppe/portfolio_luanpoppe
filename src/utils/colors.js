69644faa4ceb057b4bdd03211b05cc4d054c3969
luanpoppe <118562068+luanpoppe@users.noreply.github.com>
2023-08-09 11:10:01 -0300

    initial commit - v02

--- src/utils/colors.js
+++
@@ -1,8 +0,0 @@
-// https://coolors.co/36393b-a5d8ff-e4eff1-bfb6bb-c49799
-// https://coolors.co/36393b-1f9eff-e4eff1-bfb6bb-c49799
-
-export const white = "#E4EFF1"
-export const lightPink = "#C49799"
-export const black = "#36393B"
-export const blue = "#1F9EFF"
-export const purple = "#BFB6BB"
